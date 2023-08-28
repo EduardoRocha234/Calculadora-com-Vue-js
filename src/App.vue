925e77a04f140fd699f0227d4deba239fc6dbcb9
Eduardo Aleixo <eduardo.aleixo@el.com.br>
2023-08-28 19:49:19 -0300

    refact(project): refactor all project for typescript

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,44 @@
-<template>
-  <div id="app">
-    <Calculadora/>
-  </div>
-</template>
-
-<script>
+<script setup lang="ts">
 import Calculadora from './components/Calculadora.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Calculadora,
-  }
-}
 </script>
 
+<template>
+	<div>
+		<a
+			href="https://vitejs.dev"
+			target="_blank"
+		>
+			<img
+				src="/vite.svg"
+				class="logo"
+				alt="Vite logo"
+			/>
+		</a>
+		<a
+			href="https://vuejs.org/"
+			target="_blank"
+		>
+			<img
+				src="./assets/vue.svg"
+				class="logo vue"
+				alt="Vue logo"
+			/>
+		</a>
+	</div>
+	<Calculadora msg="Vite + Vue" />
+</template>
+
 <style scoped>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  height: auto;
-  width: auto;
-
+.logo {
+	height: 6em;
+	padding: 1.5em;
+	will-change: filter;
+	transition: filter 300ms;
 }
-
-img {
-   width:5%;
-   margin: 10px 57px 10px 10px; 
-   float: none;
+.logo:hover {
+	filter: drop-shadow(0 0 2em #646cffaa);
 }
-
+.logo.vue:hover {
+	filter: drop-shadow(0 0 2em #42b883aa);
+}
 </style>
--- src/components/Calculadora.vue
+++ src/components/Calculadora.vue
@@ -1,194 +1,216 @@
 <template>
-  <div class="calculadora">
-  <div  class="telaDaCalculadora">{{valorDigitado ||'0' }} {{ simboloDaOperação }} </div>
-  <div @click="limpar" class="botao">C</div>
-  <div @click="sinalDeMenos" class="operador">+/-</div>
-  <div @click="porcentagem" class="operador">%</div>
-  <div @click="dividir" class="operador">÷</div>
-  <div @click="adicionarNumero('7')" class="botao">7</div>
-  <div @click="adicionarNumero('8')" class="botao">8</div>
-  <div @click="adicionarNumero('9')" class="botao">9</div>
-  <div @click="multiplicar" class="operador">X</div>
-  <div @click="adicionarNumero('4')" class="botao">4</div>
-  <div @click="adicionarNumero('5')" class="botao">5</div>
-  <div @click="adicionarNumero('6')" class="botao">6</div>
-  <div @click="subtrair" class="operador">-</div>
-  <div @click="adicionarNumero('1')" class="botao">1</div>
-  <div @click="adicionarNumero('2')" class="botao">2</div>
-  <div @click="adicionarNumero('3')" class="botao">3</div>
-  <div @click="somar" class="operador">+</div>
-  <div @click="adicionarNumero(0)" class="zero">0</div>
-  <div @click="ponto" class="botao">.</div>
-  <div @click="resultado" class="operador">=</div>
-
-  </div>
+	<div style="display: flex; justify-content: center; align-items: center">
+		<div class="calculator">
+			<div class="telaDaCalculadora">
+				{{ typeValue || '0' }} {{ operatorSimbol }}
+			</div>
+			<div
+				v-for="(button, idx) in buttons"
+				:key="idx"
+				@click="button.func"
+        :class="button.class"
+			>
+				{{ button.text }}
+			</div>
+		</div>
+	</div>
 </template>
 
-<script>
-export default {
-  data: function() {
-    return {
-      valorDigitado: '',
-      valorAnterior: null,
-      clicouNoOperador: false,
-      calculo: null,
-      simboloDaOperação: ''
-    }
-  },
-  methods: {
-    limpar() {
-      this.valorDigitado = '';
-      this.simboloDaOperação = ''
-    },
-    //para adicionar o numeros na tela
-      adicionarNumero(numero) {
-        if (this.clicouNoOperador) {
-          this.valorDigitado = '';
-          this.clicouNoOperador = false;
-        }
-  
-        this.valorDigitado = `${this.valorDigitado}${numero}`;
-      },
+<script lang="ts" setup>
+import {ref} from 'vue'
 
-    sinalDeMenos() {
-      //verifica se o primeiro inice  tem o sinal se tiver retorna o segundo indice
-      //que seria o número q está a frente do sinal, se n tiver ele retorna com o sinal
-      this.valorDigitado = this.valorDigitado.charAt(0) === '-'
-        ? this.valorDigitado.slice(1)
-        : `-${this.valorDigitado}`;
-    },
+const buttons = [
+  {text: 'C', func: () => clear(), class: 'button'},
+  {text: '+/-', func: () => minusSign(), class: 'operator'},
+  {text: '%', func: () => percentage(), class: 'operator'},
+  {text: '÷', func: () => divide(), class: 'operator'},
+  {text: '7', func: () => addNumber('7'), class: 'button'},
+  {text: '8', func: () => addNumber('8'), class: 'button'},
+  {text: '9', func: () => addNumber('9'), class: 'button'},
+  {text: 'X', func: () => mult(), class: 'operator'},
+  {text: '4', func: () => addNumber('4'), class: 'button'},
+  {text: '5', func: () => addNumber('5'), class: 'button'},
+  {text: '6', func: () => addNumber('6'), class: 'button'},
+  {text: '-', func: () => subtract(), class: 'operator'},
+  {text: '1', func: () => addNumber('1'), class: 'button'},
+  {text: '2', func: () => addNumber('2'), class: 'button'},
+  {text: '3', func: () => addNumber('3'), class: 'button'},
+  {text: '+', func: () => add(), class: 'operator'},
+  {text: '0', func: () => addNumber(0), class: 'zero'},
+  {text: '.', func: () => point(), class: 'button'},
+  {text: '=', func: () => result(), class: 'operator'},
+]
 
-    //ele verifica se á o ponto na operação abaixo pois indexOf retorna -1 caso o mesmo
-    //não esteja presente. sendo assim se for iggual a -1 significa que não á ponto
-    //e então ele adiciona o ponto a tela da calculadora
-      ponto() {
-        if (this.valorDigitado.indexOf('.') === -1) {
-          this.adicionarNumero('.');
-        }
-      },
-      
-    porcentagem() {
-      this.valorDigitado = `${parseFloat(this.valorDigitado) / 100}`;
-    },
+const typeValue = ref<string>('')
+const previousValue = ref<string>('')
+const clickOperator = ref<boolean>(false)
+const calc = ref<(num1: number, num2: number) => number>()
+const operatorSimbol = ref<string>('')
 
-    //operação para quando clicar o botão o valor atual vire o valor antigo para 
-    //que tenha os dois valores para a operação
-    setarValor() {
-      this.valorAnterior = this.valorDigitado;
-      this.clicouNoOperador = true;
-    },
+const clear = (): void => {
+	typeValue.value = ''
+	operatorSimbol.value = ''
+}
 
-    dividir() {
-      this.simboloDaOperação = '÷'
-      this.calculo = (num1, num2) => num1 / num2;
-      this.setarValor();
-    },
-    multiplicar() {
-      this.simboloDaOperação = 'x'
+const addNumber = (newNumber: number | string): void => {
+	if (clickOperator.value) {
+		typeValue.value = ''
+		clickOperator.value = false
+	}
 
-      this.calculo = (num1, num2) => num1 * num2;
-      this.setarValor();
-    },
-    subtrair() {
-      this.simboloDaOperação = '-'
-      this.calculo = (num1, num2) => num1 - num2;
-      this.setarValor();
-    },
-    somar() {
-      this.simboloDaOperação = '+'
+	typeValue.value = `${typeValue.value}${newNumber}`
+}
 
-      this.calculo = (num1, num2) => num1 + num2;
-        this.setarValor();
-    },
-     resultado() {
-       this.simboloDaOperação = ''
-       this.valorDigitado = `${this.calculo(
-               parseFloat(this.valorAnterior),
-               parseFloat(this.valorDigitado),
-      )}`;
-      this.valorAnterior = null;
-    },
-    },
-  }
+const minusSign = (): void => {
+	//verifica se o primeiro inice  tem o sinal se tiver retorna o segundo indice
+	//que seria o número q está a frente do sinal, se n tiver ele retorna com o sinal
+	typeValue.value =
+		typeValue.value.charAt(0) === '-'
+			? typeValue.value.slice(1)
+			: `-${typeValue.value}`
+}
 
+//ele verifica se á o ponto na operação abaixo pois indexOf retorna -1 caso o mesmo
+//não esteja presente. sendo assim se for iggual a -1 significa que não á ponto
+//e então ele adiciona o ponto a tela da calculator
+const point = (): void => {
+	if (typeValue.value.indexOf('.') === -1) {
+		addNumber('.')
+	}
+}
+
+const percentage = (): void => {
+	if (!!typeValue.value) {
+		typeValue.value = `${parseFloat(typeValue.value) / 100}`
+	}
+}
+
+//operação para quando clicar o botão o valor atual vire o valor antigo para
+//que tenha os dois valores para a operação
+const setValue = (): void => {
+	previousValue.value = typeValue.value
+	clickOperator.value = true
+}
+
+const divide = (): void => {
+	operatorSimbol.value = '÷'
+	calc.value = (num1, num2) => num1 / num2
+	setValue()
+}
+
+const mult = (): void => {
+	operatorSimbol.value = 'x'
+	calc.value = (num1, num2) => num1 * num2
+	setValue()
+}
+
+const subtract = (): void => {
+	operatorSimbol.value = '-'
+	calc.value = (num1, num2) => num1 - num2
+	setValue()
+}
+
+const add = (): void => {
+	operatorSimbol.value = '+'
+	calc.value = (num1, num2) => num1 + num2
+	setValue()
+}
+
+const result = (): void => {
+	operatorSimbol.value = ''
+	if (calc.value) {
+		const calcResult: number = calc.value(
+			parseFloat(previousValue.value),
+			parseFloat(typeValue.value)
+		)
+		typeValue.value = `${calcResult}`
+		previousValue.value = ''
+	}
+}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-
-.calculadora {
-  background-color: rgba(14, 187, 144, 0.945);
-  margin: auto  auto 45%;
-  float: center;
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-auto-rows: minmax(50px, auto);
-  width: 395px;
-  border-radius:  30px;
-
+.calculator {
+	display: grid;
+	grid-template-columns: repeat(4, 1fr);
+	grid-auto-rows: auto;
+	width: 400px;
+	border-radius: 30px;
+	box-shadow: 30px rgb(0, 0, 0);
+	background: rgba(14, 187, 144, 0.945);
+	padding-bottom: 5px;
 }
 
 .telaDaCalculadora {
-  height: 350px;
-  width: 357px;
-  border-radius: 175px;
-  grid-column: 1 / 5;
-  background-color: rgb(8, 230, 163);
-  color: rgb(0, 0, 0);
-  text-align: center;
-  padding: 15px;
-  border-color: rgb(18, 136, 106);
-  border: 2px solid rgb(47, 121, 102);
-  height: 50px;
-  font-size: 40px;
+	height: 350px;
+	width: 357px;
+	border-radius: 25px 25px 10px 10px;
+	grid-column: 1 / 5;
+	background-color: rgb(8, 230, 163);
+	color: rgb(0, 0, 0);
+	text-align: center;
+	padding: 15px;
+	margin-bottom: 2px;
+	margin-top: 2px;
+	margin-left: 4px;
+	border-color: rgb(18, 136, 106);
+	border: 2px solid rgb(47, 121, 102);
+	height: 50px;
+	font-size: 40px;
 }
 .zero {
-  height: 45px;
-  width: 157px;
-  border-radius: 48px;
-  font-size: 35px;
-  padding: 22px 15px 15px 15px;
-  grid-column: 1 / 3;
-  text-align: center;
-  border: 1px solid rgb(35, 58, 49);
-  background-color: rgb(177, 255, 232);
+	font-weight: bolder;
+	height: 45px;
+	width: 157px;
+	border-radius: 48px;
+	font-size: 35px;
+	padding: 22px 15px 15px 15px;
+	margin: auto;
+	grid-column: 1 / 3;
+	text-align: center;
+	border: 1px solid rgb(35, 58, 49);
+	background: rgb(177, 255, 232);
+	color: black;
 }
 .zero:hover {
-  background-color: rgb(137, 243, 213);
+	background-color: rgb(137, 243, 213);
 }
 
-.botao {
-  height: 48px;
-  width: 48px;
-  border-radius: 24px;
-  font-size: 30px;
-  padding: 22px 15px 15px 22px;
-  text-align: center;
-  border: 1px solid rgb(0, 0, 0);
-  background-color: rgb(177, 255, 236);
-  margin: auto;
-  
+.button {
+	height: 48px;
+	width: 48px;
+	border-radius: 24px;
+	font-size: 30px;
+	font-weight: bolder;
+	display: flex;
+	text-align: center;
+	justify-content: center;
+	padding: 18px 15px 15px 18px;
+	border: 1px solid rgb(0, 0, 0);
+	background: rgb(177, 255, 236);
+	color: black;
+	cursor: pointer;
+	margin: auto;
 }
 
-.botao:hover{
-  background-color: rgb(137, 243, 213);
+.button:hover {
+	background-color: rgb(137, 243, 213);
 }
-.operador {
-  margin: auto;
-  height: 48px;
-  width: 48px;
-  border-radius: 24px;
-  color: #fff;
-  padding: 18px 15px 15px 18px;
-  text-align: center;
-  font-size: 30px;
-  border: 1px solid rgb(57, 92, 78);
-  background-color: rgb(18, 136, 106);
-}
-
-.operador:hover{
-  background-color: rgb(47, 121, 102);
+.operator {
+	margin: auto;
+	height: 48px;
+	width: 48px;
+	border-radius: 24px;
+	color: #cbdbcf;
+	padding: 18px 15px 15px 18px;
+	font-weight: bold;
+	text-align: center;
+	font-size: 30px;
+	border: 1px solid rgb(57, 92, 78);
+	background: rgb(18, 136, 106);
 }
 
+.operator:hover {
+	background: rgb(47, 121, 102);
+}
 </style>
